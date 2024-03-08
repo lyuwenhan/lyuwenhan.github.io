3364a688c9669043790969bbc861a857916f0baa
lyuwenhan <lyuwenhan@gmail.com>
2024-03-08 20:42:59 +0800

    ...

---
+++ cssjs/a.js
@@ -0,0 +1,6 @@
+function jump() {
+	var currentPath = window.location.pathname;
+	var newUrl = "https://lanpingzhong-fishing.github.io/" + currentPath;
+	window.location.href = newUrl;
+	return false; // 防止链接的默认行为（即跳转到锚点）
+}

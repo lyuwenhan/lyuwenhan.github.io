0598b8a764b7499783a0022327dc3b5babfcc9fa
lyuwenhan <lyuwenhan@gmail.com>
2024-03-13 21:48:51 +0800

    ...

--- cssjs/a.js
+++ cssjs/a.js
@@ -4,3 +4,13 @@ function jump() {
 	window.location.href = newUrl;
 	return false; // 防止链接的默认行为（即跳转到锚点）
 }
+function get() {
+	$.getJSON('https://lyuwenhan.github.io/file/fishing.json', function(data) {
+	  // 在请求成功后执行的回调函数
+	  console.log('Received data:', data);
+	})
+	.fail(function(jqXHR, textStatus, errorThrown) {
+	  // 在请求失败时执行的回调函数
+	  console.error('Error fetching data:', textStatus, errorThrown);
+	});
+}

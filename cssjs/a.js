f5b94eaf8ee4aef145d0181e66c22f03a4635b0a
lyuwenhan <lyuwenhan@gmail.com>
2024-03-18 22:35:20 +0800

    ...

--- cssjs/a.js
+++ cssjs/a.js
@@ -4,3 +4,18 @@ function jump() {
 	window.location.href = newUrl;
 	return false;
 }
+function remove_to_back(url) {
+    var segments = url.split('/');
+    if (segments.length <= 1) {
+        return url;
+    }
+    segments.pop();
+    return segments.join('/');
+}
+
+function jump_back() {
+	var currentPath = window.location.href;
+	console.log(remove_to_back(remove_to_back(currentPath)));
+	window.location.href = "https://lyuwenhan.github.io" + remove_to_back(remove_to_back(currentPath));
+	return false;
+}

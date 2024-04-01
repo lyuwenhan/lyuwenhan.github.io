5e50d036e9e7f9b40b17f904bf50f19303d479c6
lyuwenhan <lyuwenhan@gmail.com>
2024-04-01 21:44:07 +0800

    ...

---
+++ lyuwenhan.github.io-main/README.md
@@ -0,0 +1,2 @@
+open the `using method.txt` to check how to use it.
+use ubuntu(linux) to run it.
---
+++ lyuwenhan.github.io-main/cssjs/a.js
@@ -0,0 +1,21 @@
+function jump() {
+	var currentPath = window.location.pathname;
+	var newUrl = "https://lanpingzhong-fishing.github.io" + currentPath;
+	window.location.href = newUrl;
+	return false;
+}
+function remove_to_back(url) {
+    var segments = url.split('/');
+    if (segments.length <= 1) {
+        return url;
+    }
+    segments.pop();
+    return segments.join('/');
+}
+
+function jump_back() {
+	var currentPath = window.location.href;
+	console.log(remove_to_back(remove_to_back(currentPath)));
+	window.location.href = remove_to_back(remove_to_back(currentPath));
+	return false;
+}

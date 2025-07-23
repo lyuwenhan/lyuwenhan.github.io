bc98d82c7e7b066c715a6fc5df726509c74f4489
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2025-07-23 13:51:56 -0400

    edited the website

--- cssjs/a.js
+++ cssjs/a.js
@@ -1,15 +1,3 @@
-function remove_to_back(url) {
-    var segments = url.split('/');
-    if (segments.length <= 1) {
-        return url;
-    }
-    segments.pop();
-    return segments.join('/');
-}
-
-function jump_back() {
-	var currentPath = window.location.href;
-	console.log(remove_to_back(remove_to_back(currentPath)));
-	window.location.href = remove_to_back(remove_to_back(currentPath));
-	return false;
+function jump(){
+	window.location.href = window.location.pathname.replace(/\/\/+/g,"/").replace(/\/[^/]+\/?$/, "/");
 }

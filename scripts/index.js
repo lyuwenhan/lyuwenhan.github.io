1918878d42965244e3eb12220049555f2a0befec
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2026-03-27 21:55:15 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -74,6 +74,7 @@ function walk(dir) {
 	}
 }
 fs.renameSync("extensions/minecraft-java/data/next_steps.md", "extensions/minecraft-java/next_steps.md");
+fs.copyFile("LICENSE", "LICENSE.txt");
 for (const entry of fs.readdirSync("extensions", {
 		withFileTypes: true
 	}) || []) {
